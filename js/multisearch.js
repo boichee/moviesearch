26a3c2d6c09318db8143c9da8199de75e2582e0a
Boichee <bhlevenson@gmail.com>
2014-09-10 21:49:34 -0700

    Consolidated Control Functions
    On the mixed jQuery/Javascript side, doSearch() is now the single
    control function for both search buttons. Also updated search button
    text to be in Google style.
    
    TODO: Update output of JSON data received from API so its nicer looking
    and arranged in some sort of layout that makes sense.

--- js/multisearch.js
+++ js/multisearch.js
@@ -27,148 +27,156 @@ function isStringEmpty(str) {
 }
 
 // Searches for any movies matching the input string
-function doSearch()
+function doSearch(imdbID)
 {	
 	
-	// Clear the #searchResults before every request
+	// Clear the #searchResults before every new request
 	$("#searchResults").html("");
-	
-	// Set up the regex to find the year
-	var yearEx = /(19|20)[0-9]{2}/;
 	
 	// Set up the object to hold the query results until the API call
 	// Key for each property of oMovieSearch will match parameters in the OMDB API
 	var oMovieSearch = new Object();
 	
+	// Set up the regex to find the year
+	var yearEx = /(19|20)[0-9]{2}/; // Will match years between 1900-2099
+	
 	// Init a query string 
 	var apiQuery = "";
 	
 	// We're going to get the value of the form field, create an array to store title, year, or both.
-	var form = document.getElementById('searchForm');
+	var form = $('#searchForm').get(0);  // document.getElementById('searchForm');
 	var query = form.movieQuery.value;
 	form.movieQuery.value = ""; // Clear the field now that we've captured the search
 	
-	// First, make sure a search was entered.
-	if (isStringEmpty(query)) return; // if empty string found, return and do nothing.
 	
-	// Moving on....
-	
-	
-	// Test for a year within the query
-	if (yearEx.test(query)) { // If true, replace the year with an empty string and assign the year to our call object
-		aYears = yearEx.exec(query);
-		oMovieSearch.y = aYears[0]; // get the 0th index of the returned array.
-		
-		var title = query.replace(yearEx, "");
-		if (!isStringEmpty(title)) {
-			oMovieSearch.s = title.trim();
+	// Check to see if imdbID has been set, if it has, then update the settings appropriately.
+	if(imdbID != null) {
+		if (imdbID == "t") {
+			titleKey = 't';
 		} else {
-			alert ("You must enter a title in addition to a year. Please try again.");
-			return;
-		}
-	} else { // If no year entered, just apply the query to the title and set the year to null
-		oMovieSearch.s = query.trim();
-	}
-	
-	// Now create the query string by going through the object.
-	for (key in oMovieSearch) {
-		// Check that the key's value is non-null, and if so add the relevant string
-		if (oMovieSearch[key]) {
-			if (apiQuery.length > 0) apiQuery = apiQuery + "&"; // If apiQuery has something in it, then we're past the first item
+			apiQuery = "i=" + encodeURIComponent(imdbID);		
+		} 
+	} else {
+		titleKey = 's';
 			
-			// Now add the relevant parameter and value to the apiRequest
-			apiQuery = apiQuery + key + "=" + oMovieSearch[key];
-		}
 	}
 	
+	
+	// Now, check that apiQuery is still empty
+	if (apiQuery.length == 0) { // If still, empty, then we don't have a specified imdbID, so build the query.
+	
+		// First, make sure a search was entered.
+		if (isStringEmpty(query)) return; // if empty string found, return and do nothing.
+	
+		// Moving on....
+	
+	
+		// Test for a year within the query
+		if (yearEx.test(query)) { // If true, replace the year with an empty string and assign the year to our call object
+			aYears = yearEx.exec(query);
+			oMovieSearch.y = aYears[0]; // get the 0th index of the returned array.
+		
+			var title = query.replace(yearEx, "");
+			if (!isStringEmpty(title)) {
+				oMovieSearch[titleKey] = title.trim();
+			} else {
+				alert ("You must enter a title in addition to a year. Please try again.");
+				return;
+			}
+		} else { // If no year entered, just apply the query to the title and set the year to null
+			oMovieSearch[titleKey] = query.trim();
+		}
+	
+		// Now create the query string by going through the object.
+		for (key in oMovieSearch) {
+			// Check that the key's value is non-null, and if so add the relevant string
+			if (oMovieSearch[key]) {
+				if (apiQuery.length > 0) apiQuery = apiQuery + "&"; // If apiQuery has something in it, then we're past the first item
+			
+				// Now add the relevant parameter and value to the apiRequest
+				apiQuery = apiQuery + key + "=" + encodeURIComponent(oMovieSearch[key]);
+			}
+		}
+	
+	
+	}
 	// We should now have a correctly formatted query string, so build the complete API request URI
-	
-	
+
 	var requestURI = apiURI + apiQuery;
-	
 	alert(requestURI);
+
+	
 	
 	$.getJSON( requestURI, 
 		
 		function( data ) {
-	  	  	var items = [];
-	  	  	$.each( data, 
-				function( key, val ) {
-					
-					if(val == "False") {
-						alert ("No movies could be found based on your query. Try again.");
-						return;
-					}
-					
-					$.each( val, function (key, val) {
-						/*
-						Title: "Batman Begins"
-						Type: "movie"
-						Year: "2005"
-						imdbID: "tt0372784""/?ref_=fn_al_tt_1" */
-						
-						items.push("<li id='mov_" + val.imdbID + "'>" + "<a href='javascript:doReturn(\"" + val.imdbID + "\")'>"
-						+ val.Title + " (" + val.Year + ")" + "</a>" + "</li>");
-						
-					} 
-				);
-					
-	    			
-	  		  	}
-			);
-	  		
-			$( "<ul/>", {
+			
+			if (data.Search) { // That is, if a search with multiple results has been returned
 				
-	    		"class": "my-new-list",
-	    		html: items.join( "" )	}
+				layoutSearchResults(data);
 				
-			).appendTo( "#searchResults" );
+			} else { // If we only have a single object to parse through.
+				
+				layoutSingleMovie(data);
+				
+			}
+			
+	  	  	
 		}
 	);
 	
 }
 
-// Returns a single movie from the API when user clicks "I'm feelin' lucky!"
-function doReturn(imdbID)
-{
-	// Clear the #searchResults before every request
-	$("#searchResults").html("");
-	
-	
-	var form = document.getElementById('searchForm');
-	
-	var title = form.movieQuery.value;
 
-	
-	var requestString = "?t=" + encodeURIComponent(title);
-	
-	// If an imdbID string was passed (by a search link) then use that instead of what's in the form field.
-	if(imdbID != null) requestString = "?i=" + encodeURIComponent(imdbID);
-	
-	
-	var urlToSend = "http://www.omdbapi.com/" + requestString;
-	
-	alert(urlToSend);
-	
-	$.getJSON( urlToSend, 
-		
-		function( data ) {
-	  	  	var items = [];
-	  	  	$.each( data, 
-				function( key, val ) {
-	    			items.push( "<li id='" + key + "'>" + key + ": " + val + "</li>" );
-	  		  	}
-			);
-	  		
-			$( "<ul/>", {
+
+function layoutSearchResults (data)
+{
+  	var items = [];
+  	$.each( data, 
+		function( key, val ) {
+			
+			if(val == "False") {
+				alert ("No movies could be found based on your query. Try again.");
+				return;
+			}
+			
+			
+			$.each( val, function (key, val) {
 				
-	    		"class": "my-new-list",
-	    		html: items.join( "" )	}
+				items.push("<li id='mov_" + val.imdbID + "'>" + "<a href='javascript:doSearch(\"" + val.imdbID + "\")'>"
+				+ val.Title + " (" + val.Year + ")" + "</a>" + "</li>");
 				
-			).appendTo( "#searchResults" );
-		}
+			} 
+		);
+			
+			
+	  	}
 	);
 	
+	$( "<ul/>", {
+		
+		"class": "my-new-list",
+		html: items.join( "" )	}
+		
+	).appendTo( "#searchResults" );
+}
+
+
+function layoutSingleMovie(data)
+{
+  	var items = [];
+  	$.each( data, 
+		function( key, val ) {
+			items.push( "<li id='" + key + "'>" + key + ": " + val + "</li>" );
+	  	}
+	);
+	
+	$( "<ul/>", {
+		
+		"class": "my-new-list",
+		html: items.join( "" )	}
+		
+	).appendTo( "#searchResults" );
 }
 
 
@@ -239,7 +247,7 @@ function ajaxByHand()
 // Attach an event handler to the search button
 
 $(document).ready(function() {
-	$('#movieReturn').click(function() { doReturn() });
+	$('#movieReturn').click(function() { doSearch('t') });
 	$('#movieSearch').click(function() { doSearch() });
 });
 
