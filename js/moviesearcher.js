3b2efdccaebbaeb400181ccef8c01ee1e8066c61
Boichee <bhlevenson@gmail.com>
2014-09-12 15:36:52 -0700

    Different, objectified implementation
    Reworked the implementation to make it conform more to MVC and to make
    it more scalable and maintainable. In MovieSearcher.js, the user’s
    inputs are abstracted into a model object (omdbSearch) that
    communicates with the API.
    
    Additionally, each new parameter is represented by another object I’ve
    created called omdbSearchArg, which holds the a human readable name (or
    explanation) of the argument, the argKey (as specified by the omdbAPI)
    and the argValue (user’s entry). Each parameter object is then stored
    in an array in the omdbSearch object (args).
    
    The omdbSearch object is itself really a singleton in concept, with a
    single global variable in the script providing the instance. Were it
    possible in javascript, I would make the arguments array private,
    declare the variable as static, and provide getter methods that return
    an immutable version of the array. In the meantime, I think I’ll have
    to add some error checking to try to prevent the creation of more than
    one instance of the object.
    
    On the controller side, I need to build something more robust, but for
    the moment, I’ve simply added a setInterval() to the “searchOMDB”
    function that is called when the user clicks “search” (or presses
    enter). As such, there is no code in the omdbSearch object itself to
    communicate with a view. In effect, the controller loads the necessary
    params into the model, tells the model to execute the search, and then
    (by using setInterval and a callback) waits until the search has been
    completed (by checking bSearchComplete in the model). Once the search
    is complete, the controller’s callback ( “outputData()” ) clears the
    interval and passes control (and the relevant data to output) to the
    “View” (or really View Controller, I suppose this is more MVVC),
    “layoutSearchResults()” which converts the data into HTML and appends
    it to the DOM.
    
    TODO:
    1. Still need to implement 2 other views:
    a. The “selected movie” view
    b. The “full plot” in “selected movie” view.
    
    2. Also need to re-implement the convenience code that checks the
    number of search results and if that # is 1, moves directly to the
    “selected movie” view.
    
    3. I need to add the modal user error notifications back in and improve
    the validation methods in omdbSearch. At the moment, it validates both
    the Title and Year fields but wouldn’t have a way of distinguishing for
    the view controller which of the two fields was the caused the error
    (if not both.)
    
    4. Need to add a complete pushState implementation at some point so
    that back and forward browser functionality are possible.
    
    5. At some point, I should probably abstract the views into Handlebars
    to improve upon my current method of updating search results/or
    selected movie views through insertion of a large amount of messy and
    golfed HTML into the DOM via jQuery’s append function. Would definitely
    make it easier to replace individual aspects of a result if desired at
    some point.
    
    6. Need to re-implement sorting of search results. (And give the UX on
    the sorting a little love too). Oh, and animate the sort!
    
    7. Some stuff I’m probably not thinking of right now.

---
+++ js/moviesearcher.js
@@ -0,0 +1,242 @@
+// Object oriented implementation of MovieSearcher
+
+// Object-based implementation
+
+// Define a search object
+
+var intervalID;
+
+function omdbSearch () {
+    this.args = []; // An array to hold omdbSearchArgument
+    this.searchResults = {}; // Will hold the results of the search as an object (once parsed from JSON)
+    this.regExForDate = /(?:19|20)\d{2}/; // for date string validation: makes sure date is 4 chars and between 1900-2099
+    this.bSearchComplete = false; // Flag to indicate whether or not a search has been run.
+}
+ 
+omdbSearch.prototype = {
+
+    createQuery: function() {
+      // Creates a complete GET string out of all arguments available
+      var separator = "?";
+      var query = "";
+      for (i = 0; i < this.args.length ; i++ ) {
+      	if (i == 1) separator = "&";
+      	query = query + separator + this.args[i].createArgString();
+      }
+
+      return "http://www.omdbapi.com/" + query;
+    },
+
+    createArg: function(argName, argKey, argValue) {
+    	// Convenience function, creates a new omdbSearchArg and then adds to array
+    	var oArg = new omdbSearchArg(argName, argKey, argValue);
+    	this.args[this.args.length] = oArg;
+    },
+
+    addArg: function(oArg) {
+    	// Adds an existing instance of omdbSearch Arg to the array
+
+    	// First check to make sure oArg is an omdbSearchArg
+    	if (oArg instanceof omdbSearchArg) {
+    		this.args[this.args.length] = oArg;
+    	} else {
+    		// passed object is not an omdbSearchArg instance, throw exception
+    		throw "Passed object must be an instance of omdbSearchArg";
+    	}
+    	
+    },
+
+    // Validation of Title, Date inputs
+    // Title must be at least 3 characters
+    validateArgs: function() {
+
+    	for (var i = 0; i < this.args.length; i++) {
+    		if (this.args[i].argName == "Title") {
+    			// If we're looking at the title arg, make sure it's at least 3 chars long
+    			return (this.args[i].argValue.length >= 3);
+    		} 
+
+    		if (this.args[i].argName == "Year") {
+    			// Prep some date objects to compare against
+				var latestDate = new Date(Date.now() + ((60 * 60 * 24 * 365) * 1000)); // Get next year
+				var earliestDate = new Date(); earliestDate.setFullYear(1900); // Set the earliest date allowed to 1900
+
+				// Set up a date object for the year entered by the user
+				var enteredDate = new Date(); enteredDate.setFullYear(this.args[i].argValue);
+
+				// Set up boolean expression to check against
+				// true if year entered is between 1900 and next year
+				return (enteredDate.getTime() > earliestDate.getTime() &&  
+					enteredDate.getTime() < latestDate.getTime());
+
+    		}
+    	}
+
+    	return true; // If we're here, then no errors found
+    },
+
+    searchAPI: function() {
+    	$.getJSON(this.createQuery(), function( data ) {
+    		MovieSearcher.searchResults = data;
+
+    		// Once data has been returned, mark flag to true so DOM functions can take over
+    		MovieSearcher.bSearchComplete = true; 
+    	});
+
+    },
+
+    // Convenience function, grabs the appropriate field from the result dataset
+    getSearchResults: function() {
+    	if (this.bSearchComplete) // Make sure a search has been run.
+    		return this.searchResults.Search; // If search run, pull the "Search" array from the results and return.
+    	else {
+    		throw "You haven't run the API search yet or results have not yet been returned!";
+    		return undefined;
+    	}
+    }
+
+
+
+
+
+
+};
+
+function omdbSearchArg (argName, argKey, argValue) {
+	this.argName = argName;
+	this.argKey = argKey;
+	this.argValue = argValue;
+}
+
+omdbSearchArg.prototype = {
+	createArgString: function() {
+		// Converts to GET format (x=someValue)
+		return this.argKey + "=" + encodeURIComponent(this.argValue);
+	}
+};
+
+// Now set up the procedural part of the program
+// 1. Get entered values from form fields
+// 2. Add those values to the search object
+
+function outputToConsole() {
+	var searchResults = MovieSearcher.getSearchResults();
+
+	for (var i = 0; i < searchResults.length; i++) {
+			console.log("Result " + i + ":");
+			console.log("Title: " + searchResults[i].Title);
+			console.log("Year: " + searchResults[i].Year);
+			console.log("imdbID: " + searchResults[i].imdbID);
+			console.log("\n");
+	}
+}
+
+function outputData() {
+	if (MovieSearcher.bSearchComplete) {
+		// If the search has been completed, then update the DOM
+		clearInterval(intervalID);
+		layoutSearchResults(MovieSearcher.getSearchResults());
+	
+	};
+
+}
+
+// This function does the layout of the search results
+function layoutSearchResults (dataToProcess)
+{
+	
+	// Prep the search results for display
+	var counter = 0; // set up an iterative counter to start a row every 3 results
+  	var items = [];
+  	$.each( dataToProcess, 
+		function( key, val ) {
+			
+			// Check what iteration we're at and add the appropriate row divs
+			if ((counter != 0) && (counter % 3 == 0)) items.push('</div>');
+			if (counter % 3 == 0) items.push('<div class="row">');
+			counter++;
+			
+			items.push('<div class="col-md-4"><h2><a href="javascript:continueSearch(false, false, \''+val.imdbID+'\')">'+val.Title+'</a></h2><p>'+val.Year+'</p><p><a class="btn btn-default" href="javascript:continueSearch(false, false, \''+val.imdbID+'\')" role="button" target="_blank">Select this movie &raquo;</a></p></div>');	
+	  	});
+		
+	// Now prep the display about the search itself
+	var queryString = "";
+	var searchTitle = $('#frm_movieQuery').get(0).value;
+	var searchYear = $('#frm_movieYear').get(0).value;
+	queryString = searchTitle;
+	// Add the year if one was entered
+	queryString = (searchYear && searchYear.length > 0) ? queryString + ' (' + searchYear + ')' : queryString;
+	var queryDisplayHtml = '<h1>You searched for:</h1><h3>"'+ queryString +'"</h3><p>And got '+ counter +' results.</p>';
+	
+		// Output all prepped, so append the html in the appropriate locations on screen
+		// First make sure those elements are empty.
+		$('#mainOutput').empty();
+		$('#detailsOutput').empty();
+		
+		// Now append
+		$('#mainOutput').append(queryDisplayHtml);
+		$('#detailsOutput').append(items.join(""));
+		
+		$('#sort_dropdown').css('display', 'block');
+}
+
+function searchOMDB() {
+
+	// Initialize the global search object to its empty state;
+	MovieSearcher = new omdbSearch();
+
+	// Get the values entered into the form
+	frm_title = $('#frm_movieQuery').get(0).value;
+	frm_year = $('#frm_movieYear').get(0).value;
+
+	// Create new arg objects for each value
+	MovieSearcher.createArg('Title', 's', frm_title);
+
+	// Check to make sure a year was entered, and if so, assign
+	if (frm_year.length) {
+		MovieSearcher.createArg('Year', 'y', frm_year);
+	};
+	
+
+	// Check to make sure data was entered correctly by user, and if not, let them know
+	if (!MovieSearcher.validateArgs()) {
+		var d = new Date();
+		alert ("Your input was incorrect. Title must be at least 3 letters, and year must be between 1900 and " + d.getFullYear() + ".");
+		return false;
+	}
+
+	// Everything, ok? Then run the search!
+	MovieSearcher.searchAPI();
+
+	// Now run an interval and wait until data is received
+	intervalID = setInterval(function(){ outputData() }, 100);
+
+}
+
+
+// Event handlers to make it go!
+
+$(document).ready(function() {
+	$('#frm_doSearch').click(function() { searchOMDB(this.form) });
+	$('#frm_doSearch').dblclick(function() { searchOMDB(this.form, 'dblClick')});
+	$( "#frm_movieQuery" ).keypress(function( event ) {
+	  if ( event.which == 13 ) {
+	     event.preventDefault();
+		 searchOMDB( $('#frm_searchForm').get(0) );
+	  }
+	});
+	$( "#frm_movieYear" ).keypress(function( event ) {
+	  if ( event.which == 13 ) {
+	     event.preventDefault();
+		 searchOMDB( $('#frm_searchForm').get(0) );
+	  }
+	});
+});
+
+
+var MovieSearcher; // set up the global seach object 
+var intervalID; // set an intervalID so I can cancel it later
+
+
+
+
